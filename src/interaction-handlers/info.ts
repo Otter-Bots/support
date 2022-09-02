5172c6d1e036d60a675c1f41b01913823f38fff9
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-09-02 21:26:17 +0400

    Update info.ts

--- src/interaction-handlers/info.ts
+++ src/interaction-handlers/info.ts
@@ -12,6 +12,6 @@ export default class extends InteractionHandler {
     }
 
     public async run(interaction: ButtonInteraction) {
-        interaction.reply({content: "Otter Bots Commissions\nYou can commission us to create and host a bot/website for you.\n\nWhat do I do once I have opened a ticket?\n• Tell us what kind of bot you want (moderation, utility, etc)\n• Provide a command list (a list of all commands you want the bot to have.\n• Have patience and don't spam @mentions, opening a ticket already notifies us.", ephemeral: true})
+        interaction.reply({content: "**Otter Bots Commissions**\nYou can commission us to create and host a bot or website for you. Below are Frequently Asked Questions about commissions. \n\n**What do I do once I have opened a ticket?**\n Tell us what kind of bot you want (moderation, utility, etc) and provide a command list (a list of all commands you want the bot to have.\n\n**What is the price?**\n The price will vary on the type of bot and how big the bot is.\n\n**Where will the bot/website be hosted and will I be able to see the code?**\n The bot is hosted on a private VPS, and our uptime can be found at https://status.otterbots.xyz. As for seeing the code, all of the code for our commissions are in private repositories at https://github.com/Otter-Bots-Commissions. If you would like to see the code, we would be happy to either send it to you or give you access to the repository.", ephemeral: true})
     }
 }
